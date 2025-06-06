25575d51e80dd1816de3dc248e2531ad46dd25c4
Josende{v} <josuejosende@gmai.com>
2025-06-06 15:01:58 +0200

    Añadido adaptador netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
-import node from '@astrojs/node'
+// import node from '@astrojs/node'
+import netlify from '@astrojs/netlify'
 import tailwind from '@astrojs/tailwind'
 import vue from '@astrojs/vue'
 // import clerk from '@clerk/astro'
@@ -8,7 +9,7 @@ import vue from '@astrojs/vue'
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), vue() /* , clerk() */],
-  adapter: node({ mode: 'standalone' }),
+  adapter: netlify(),
   output: 'server',
   devToolbar: {
     enabled: false
