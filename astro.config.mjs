4cf49d03b21d5fab5c2a285e1ac5828464acc21e
Josende{v} <josuejosende@gmai.com>
2025-06-06 16:31:53 +0200

    Refactor code structure for improved readability and maintainability

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
 // import node from '@astrojs/node'
-import netlify from '@astrojs/netlify'
+import vercel from '@astrojs/vercel'
 import tailwind from '@astrojs/tailwind'
 import vue from '@astrojs/vue'
 // import clerk from '@clerk/astro'
@@ -9,7 +9,7 @@ import vue from '@astrojs/vue'
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), vue() /* , clerk() */],
-  adapter: netlify(),
+  adapter: vercel(),
   output: 'server',
   devToolbar: {
     enabled: false
